6ecfc42e4b23cbfe2b932dab03aed5ae609065b9
mavlianov20001settings <jagu68835@gmail.com>
2022-12-08 16:35:37 +0600

    hw2

---
+++ script2.js
@@ -0,0 +1,31 @@
+let arr = [];
+for (let a = 0; a < 11; a++) {
+  arr.push(a);
+}
+console.log(arr);
+//
+//
+//
+//
+//
+
+for (let x = 20; x >= 0; x--) {
+  console.log(x);
+}
+
+//
+//
+//
+//
+//
+//
+let color = String(prompt("вводите цвет "));
+if (color == "красный") {
+  alert("СТОП!");
+} else if (color == "желтый") {
+  alert("ЖДИ!");
+} else if (color == "зеленный") {
+  alert("ЕЗЖАЙ");
+} else {
+  alert("надо вводит цвета ");
+}
